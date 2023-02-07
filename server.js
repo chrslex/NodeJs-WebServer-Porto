d18cca505a7d17e4b6d4c5bd613b9aa5e38a7a6e
chrslex <alexandro.tobing@gmail.com>
2023-02-08 03:12:21 +0700

    Adding built-in, custom, and 3rd party middleware

--- server.js
+++ server.js
@@ -1,8 +1,36 @@
 const path = require('path');
 const express = require('express');
 const app = express();
+const cors = require('cors');
+const {logger} = require('./middleware/logEvents');
+const { errorHandler } = require('./middleware/errorHandler');
 
 const PORT = process.env.PORT || 3500;
+
+// custom middleware logger
+app.use(logger);
+
+// 3rd-party cors middleware
+const whitelist = ['https://www.google.com','http://localhost:3500']
+const corsOptions = {
+    origin : (origin, callback) => {
+        if(whitelist.indexOf(origin) !== -1 || !origin) {
+            callback(null, true);
+        } else {
+            callback(new Error("Not allowed by CORS"));
+        }
+    },
+    optionSuccessStatus: 200
+}
+app.use(cors(corsOptions));
+// built-in url-encoded data middleware
+app.use(express.urlencoded({extended:false}));
+
+// built-in json middleware
+app.use(express.json());
+
+// built-in static files middleware
+app.use(express.static(path.join(__dirname, "/public"))) 
 
 app.get('^/$|/index(.html)?', (req, res)=> {
     res.sendFile(path.join(__dirname, "views", "index.html"));
@@ -20,5 +48,8 @@ app.get('/old-page(.html)?', (req, res) => {
 app.get('/*', (req, res) => {
     res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
 })
+
+// custom error handling
+app.use(errorHandler);
 
 app.listen(PORT, () => console.log(`Server running on PORT ${PORT}`))
