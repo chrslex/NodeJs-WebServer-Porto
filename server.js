daca63dea16546da4b99eb2c9b2e60ceb1e5b977
chrslex <alexandro.tobing@gmail.com>
2023-02-14 02:36:39 +0700

    Add routing

--- server.js
+++ server.js
@@ -31,23 +31,24 @@ app.use(express.json());
 
 // built-in static files middleware
 app.use(express.static(path.join(__dirname, "/public"))) 
+app.use('/subdir',express.static(path.join(__dirname, "/public"))) 
 
-app.get('^/$|/index(.html)?', (req, res)=> {
-    res.sendFile(path.join(__dirname, "views", "index.html"));
-})
+// Routing for subdir
+app.use('/', require('./routes/root').router);
+app.use('/subdir', require('./routes/subdir').router);
+app.use('/employees', require('./routes/api/employees').router);
 
-app.get('/new-page(.html)?', (req, res) => {
-    res.sendFile(path.join(__dirname, "views", "new-page.html"));
 
-})
-
-app.get('/old-page(.html)?', (req, res) => {
-    res.redirect(301,'/new-page.html');
-})
-
-app.get('/*', (req, res) => {
-    res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
-})
+app.all('*', (req, res) => {
+    res.status(404);
+    if (req.accepts('html')) {
+        res.sendFile(path.join(__dirname, 'views', '404.html'));
+    } else if (req.accepts('json')) {
+        res.json({ "error": "404 Not Found" });
+    } else {
+        res.type('txt').send("404 Not Found");
+    }
+});
 
 // custom error handling
 app.use(errorHandler);
